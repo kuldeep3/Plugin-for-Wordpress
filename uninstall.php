cca99aaf066b2f7735e48cb64adb971cbc6500b7
kuldeep3 <kuldeepupreti3@gmail.com>
2019-08-13 19:53:15 +0530

    final updates 13/08/19

--- uninstall.php
+++ uninstall.php
@@ -20,6 +20,9 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 // Access the database via SQL
 global $wpdb;
+
 $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'book'");
+
 $wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+
 $wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)");
